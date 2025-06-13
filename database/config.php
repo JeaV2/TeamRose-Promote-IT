003b2dabe7e9a1d369401b3efc066ce628cf8b8d
JeaV2 <102710@glr.nl>
2025-06-13 16:20:51 +0200

    Remove logging for index.php
    No clue how this interfered with the photo uploading but it somehow did.

--- database/config.php
+++ database/config.php
@@ -5,7 +5,6 @@ require "../helpers.php";
 $db = __DIR__ . '/bingo.sqlite';
 try {
     $pdo = new PDO("sqlite:$db");
-    cinfo("Connected to database");
 } catch (PDOException $e) {
     echo $e->getMessage();
     cerror('Connection failed: ' . $e->getMessage());
