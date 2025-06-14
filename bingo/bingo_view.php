9786a1926e7fa32547901bc68edf0f9af5fc17de
JeaV2 <102710@glr.nl>
2025-06-14 18:14:10 +0200

    Fix script

--- bingo/bingo_view.php
+++ bingo/bingo_view.php
@@ -13,7 +13,7 @@ global $userTasksData;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/ootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <script src="../content/js/toggle.js"></script>
     <script src="../content/js/photo_upload.js"></script>
     <title>OHM Festival Bingo!</title>
